2ef90879c406ca1d347c07d0b2b7d2c76f9ae38c
Tim Frison <tim.frison@clio.com>
2023-11-27 23:40:53 -0700

    s/ci_check/ci_pass
    This is a glimpse into the sausage making of CI pipelines in main.
    Although I spin up my own fork, with PRs into my own branches (to keep
    main clean), I'm opting for this simpler approach as it is faster to
    iterate on.

--- .github/bin/validate.sh
+++ .github/bin/validate.sh
@@ -9,6 +9,6 @@ LANGUAGES_TO_GO=$((100 - PUBLISHED_LANGUAGE_COUNT))
 if ! grep -q "${LANGUAGES_TO_GO}_to_go" $REPO_DIR/README.md; then
   ci_error "README.md does not have the correct language count! (${LANGUAGES_TO_GO} to go!)"
 fi
-ci_check "README.md has the correct language count! (${LANGUAGES_TO_GO} to go!)"
+ci_pass "README.md has the correct language count! (${LANGUAGES_TO_GO} to go!)"
 
 
