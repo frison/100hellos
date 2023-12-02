ef467efe60f09222aa82aa71b7dd064c7239c60f
Tim Frison <frison@gmail.com>
2023-12-02 01:26:34 -0700

    Warn! Error message warning not sufficient
    In this case, the valid likely saved me from publishing 050-c to
    dockerhub. However the error message makes it sound like to fix the
    error you should update the README.md, but what really needs to be fixed
    is how the number wasn't automatically updated.

--- .github/bin/validate.sh
+++ .github/bin/validate.sh
@@ -7,7 +7,7 @@ source "$REPO_DIR/.utils/functions.sh"
 PUBLISHED_LANGUAGE_COUNT=$(published_languages | wc -l)
 LANGUAGES_TO_GO=$((100 - PUBLISHED_LANGUAGE_COUNT))
 if ! grep -q "${LANGUAGES_TO_GO}_to_go" $REPO_DIR/README.md; then
-  ci_error "README.md does not have the correct language count! (${LANGUAGES_TO_GO} to go!)"
+  ci_error "README.md does not have the correct language count! Are you unintentionally publishing an image? Or is there really ${LANGUAGES_TO_GO} to go!?"
 fi
 ci_pass "README.md has the correct language count! (${LANGUAGES_TO_GO} to go!)"
 
